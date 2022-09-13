d359ba2dee14e6287cd2d4a451f1fceb8fc06fe3
Soheil Valipour <sohxel@gmail.com>
2022-09-13 23:35:39 +0430

    add some file and prepared js for bankist app

--- script.js
+++ script.js
@@ -3,48 +3,128 @@
 
 // Coding Challenge 1_P10
 
-const poll = {
-    question: "What is your favourite programming language?",
-    options: ["0: JavaScript",
-        "1: Python",
-        "2: Rust",
-        "3: C++"],
-    // This generates [0, 0, 0, 0]. More in the next section!
-    answers: new Array(4).fill(0),
+// const poll = {
+//     question: "What is your favourite programming language?",
+//     options: ["0: JavaScript",
+//         "1: Python",
+//         "2: Rust",
+//         "3: C++"],
+//     // This generates [0, 0, 0, 0]. More in the next section!
+//     answers: new Array(4).fill(0),
 
 
-    displayResult: function () {
-        const [a0, a1, a2, a3] = this.answers;
-        console.log(`A0 : ${a0}\nA1 : ${a1}\nA2 : ${a2}\nA3 : ${a3}`);
-    },
+//     displayResult: function () {
+//         const [a0, a1, a2, a3] = this.answers;
+//         console.log(`A0 : ${a0}\nA1 : ${a1}\nA2 : ${a2}\nA3 : ${a3}`);
+//     },
 
-    registerNewAnswer: function () {
+//     registerNewAnswer: function () {
 
-        // const promtAnswer = prompt(`${this.question}\n${this.options[0]}\n${this.options[1]}\n${this.options[2]}\n${this.options[3]}`);
-        const promtAnswer = Number(
-            prompt(`${this.question}\n${this.options.join('\n')}`).trim()
-        );
+//         // const promtAnswer = prompt(`${this.question}\n${this.options[0]}\n${this.options[1]}\n${this.options[2]}\n${this.options[3]}`);
+//         const promtAnswer = Number(
+//             prompt(`${this.question}\n${this.options.join('\n')}`).trim()
+//         );
 
-        if (typeof promtAnswer === 'number' && promtAnswer < this.options.length) {
-            this.answers[promtAnswer] = this.answers[promtAnswer] + 1;
-            this.displayResult();
-        }
-        else {
-            alert('!Wrong input!');
-        }
-
-
-
-    },
+//         if (typeof promtAnswer === 'number' && promtAnswer < this.options.length) {
+//             this.answers[promtAnswer] = this.answers[promtAnswer] + 1;
+//             this.displayResult();
+//         }
+//         else {
+//             alert('!Wrong input!');
+//         }
 
 
 
-};
+//     },
 
 
 
-document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll));
+// };
+
+
+
+// document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll));
 
 // poll.displayResult();
+
+
+
+
+// -----------------Part11 Starter File------------------
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// BANKIST APP
+
+// Data
+const account1 = {
+    owner: 'Jonas Schmedtmann',
+    movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+    interestRate: 1.2, // %
+    pin: 1111,
+};
+
+const account2 = {
+    owner: 'Jessica Davis',
+    movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+    interestRate: 1.5,
+    pin: 2222,
+};
+
+const account3 = {
+    owner: 'Steven Thomas Williams',
+    movements: [200, -200, 340, -300, -20, 50, 400, -460],
+    interestRate: 0.7,
+    pin: 3333,
+};
+
+const account4 = {
+    owner: 'Sarah Smith',
+    movements: [430, 1000, 700, 50, 90],
+    interestRate: 1,
+    pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+// Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// LECTURES
+
+const currencies = new Map([
+    ['USD', 'United States dollar'],
+    ['EUR', 'Euro'],
+    ['GBP', 'Pound sterling'],
+]);
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+  /////////////////////////////////////////////////
 
 
